92476645a34e5d893203edda909731b6d7508c8e
Gabriel Francisco <gabegit2@gmail.com>
2021-01-24 15:45:28 -0300

    Created basic entities for business

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+    }
+}
