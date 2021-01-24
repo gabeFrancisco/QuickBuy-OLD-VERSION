568a224f16da05bfb4ef4f824a5a911abf117d3e
Gabriel Francisco <gabegit2@gmail.com>
2021-01-24 18:41:44 -0300

    Added EntityFramework and DbContext do the repository

---
+++ QuickBuy.Repositorio/Contexto/QuickBuyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickBuy.Dominio.Entidades;
+using QuickBuy.Dominio.ObjetosValor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Repositorio.Contexto
+{
+    public class QuickBuyDbContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<ItemPedido> ItensPedidos { get; set; }
+
+        //Objetos de valor
+        public DbSet<FormaPagamento> FormaPagamento { get; set; }
+    }
+}
