e39f0b559179d82b23e86e99601c07ae6754b517
Gabriel Francisco <gabegit2@gmail.com>
2021-01-24 16:11:30 -0300

    Updated entities and added enums

--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuickBuy.Dominio.ObjetosValor;
+using System;
 using System.Collections.Generic;
 
 namespace QuickBuy.Dominio.Entidades
@@ -8,7 +9,22 @@ namespace QuickBuy.Dominio.Entidades
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
         public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoComleto { get; set; }
+        public int NumeroEndereco { get; set; }
 
+        //Definindo as formas de pagamento
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento{ get; set; }
+
+
+        /// <summary>
+        /// Pedido deve ter pelo menos um item de pedido
+        /// ou muitos itens de pedidos
+        /// </summary>
         public ICollection<ItemPedido> ItensPedido { get; set; }
     }
 }
---
+++ QuickBuy.Dominio/ObjetosValor/FormaPagamento.cs
@@ -0,0 +1,34 @@
+﻿using QuickBuy.Dominio.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetosValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public bool IsBoleto
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+        }
+
+        public bool IsCartaoCredito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+        }
+
+        public bool IsDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+        }
+
+        public bool IsNaoDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+
+    }
+}
